e287a7b023d5e41875c1b8493b59e2a7ca21bdae
Hollowriter <rodrigodiazklipphan@gmail.com>
2023-10-27 12:06:40 -0300

    The bot can access and move around the site

--- botBrowserExplorer.py
+++ botBrowserExplorer.py
@@ -2,6 +2,7 @@ from time import sleep
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+#from instagrapi import Client
 
 def access_account():
 	global driver
@@ -10,6 +11,8 @@ def access_account():
 	driver = webdriver.Chrome(options=options)
 	driver.get('https://www.instagram.com/accounts/login/')
 	sleep(5)
+#	cl = Client()
+#	cl.login("", "")
 	user_name = driver.find_element(By.CSS_SELECTOR, "input[name='username']")
 	pass_word = driver.find_element(By.CSS_SELECTOR, "input[name='password']")
 	user_name.send_keys("")
@@ -18,15 +21,18 @@ def access_account():
 	login_link = driver.find_element(By.XPATH, "//button[@type='submit']")
 	login_link.click()
 	sleep(10)
-	sign = driver.find_element(By.CLASS_NAME, "_ac8f")
-	sign.click()
-	sleep(10)
-	notifs = driver.find_element(By.CLASS_NAME, "_a9-- _a9_1") # try to find the notifications button to click
-	notifs.click()
-	sleep(10)
-	pic = driver.find_element(By.CLASS_NAME, "kIKUG")
-	pic.click()
-	#driver.get('https://www.instagram.com/hollowriter/following/')
-	#sleep(5)
+	#sign = driver.find_element(By.CLASS_NAME, "_ac8f")
+	#sign.click()
+	#sleep(10)
+#	notifs = driver.find_element(By.CLASS_NAME, "._a9--._a9_1") # try to find the notifications button to click
+#	notifs.click()
+	#sleep(10)
+	#pic = driver.find_element(By.CLASS_NAME, "kIKUG")
+	#pic.click()
+
+def access_followers():
+	driver.get('https://www.instagram.com/hollowriter/following/')
+	sleep(5)
 
 access_account()
+access_followers()
